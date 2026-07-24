f638afa2f57a82c1f298eefbb0edeb16ecab529b
vahidpaksharifi <52237876+vahidpaksharifi@users.noreply.github.com>
2026-07-24 13:38:01 +0330

    Create tv.php

---
+++ tv.php
@@ -0,0 +1,14 @@
+{
+  "main": [
+    {
+      "name": "شبکه ۱",
+      "url": "http://yourstream.com/channel1.m3u8",
+      "flag": "news"
+    },
+    {
+      "name": "شبکه ۲",
+      "url": "http://yourstream.com/channel2.m3u8",
+      "flag": "sport"
+    }
+  ]
+}
