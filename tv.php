2ce84a64c308770d8375c534e3a6206e469a29fe
vahidpaksharifi <52237876+vahidpaksharifi@users.noreply.github.com>
2026-07-24 14:24:11 +0330

    Update tv.php

--- tv.php
+++ tv.php
@@ -1,14 +1,19 @@
 {
   "main": [
     {
-      "name": "فیلم ۱",
-      "url": "https://yourhost.com/play.php?id=1",   // ← این لینک PHP هست
-      "flag": "action"
+      "name": "شکارچی حرفه‌ای",
+      "url": "https://automaton-media.com/en/wp-content/uploads/2026/07/20250606-48784-header.jpg",
+      "flag": "https://yourhost.com/play.php?id=1"
     },
     {
-      "name": "فیلچم ۲",
-      "url": "https://yourhost.com/play.php?id=2",   // ← این لینک PHP هست
-      "flag": "comedy"
+      "name": "شب های تهران",
+      "url": "https://automaton-media.com/en/wp-content/uploads/2026/07/20250606-48784-header.jpg",
+      "flag": "https://yourhost.com/play.php?id=2"
+    },
+    {
+      "name": "داستان عشق",
+      "url": "https://automaton-media.com/en/wp-content/uploads/2026/07/20250606-48784-header.jpg",
+      "flag": "https://yourhost.com/play.php?id=3"
     }
   ]
 }
