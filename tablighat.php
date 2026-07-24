26affd73613a0ff355f831c66aa1c71086fdaa7d
vahidpaksharifi <52237876+vahidpaksharifi@users.noreply.github.com>
2026-07-24 13:57:24 +0330

    Update tablighat.php

--- tablighat.php
+++ tablighat.php
@@ -2,21 +2,21 @@
   "views": [
     {
       "name": "تبلیغ ۱",
-      "url": "https://cdn.pixabay.com/photo/2023/01/01/1200x800.jpg",
+      "url": "https://raw.githubusercontent.com/vahidpaksharifi/movies.json/refs/heads/main/images/banners/banner_1784835567944.jpg",
       "flag": "banner",
       "IsADS": true,
       "time": "5"
     },
     {
       "name": "تبلیغ ۲",
-      "url": "https://images.unsplash.com/photo-1601234567890.jpg",
+      "url": "https://raw.githubusercontent.com/vahidpaksharifi/movies.json/refs/heads/main/images/banners/banner_1784835567944.jpg",
       "flag": "banner",
       "IsADS": true,
       "time": "4"
     },
     {
       "name": "تبلیغ ۳",
-      "url": "https://cdn.pixabay.com/photo/2022/12/25/1600x900.jpg",
+      "url": "https://raw.githubusercontent.com/vahidpaksharifi/movies.json/refs/heads/main/images/banners/banner_1784835567944.jpg",
       "flag": "banner",
       "IsADS": true,
       "time": "6"
