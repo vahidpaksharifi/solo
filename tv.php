bda9ac5fefcc23ef6cd75c5e3ec2c9e51316c78d
vahidpaksharifi <52237876+vahidpaksharifi@users.noreply.github.com>
2026-07-24 14:17:42 +0330

    Update tv.php

--- tv.php
+++ tv.php
@@ -1,14 +1,14 @@
 {
   "main": [
     {
-      "name": "شبکه ۱",
-      "url": "http://yourstream.com/channel1.m3u8",
-      "flag": "news"
+      "name": "فیلم ۱",
+      "url": "https://yourhost.com/play.php?id=1",   // ← این لینک PHP هست
+      "flag": "action"
     },
     {
-      "name": "شبکه ۲",
-      "url": "http://yourstream.com/channel2.m3u8",
-      "flag": "sport"
+      "name": "فیلچم ۲",
+      "url": "https://yourhost.com/play.php?id=2",   // ← این لینک PHP هست
+      "flag": "comedy"
     }
   ]
 }
