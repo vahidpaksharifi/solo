1b38b6f8d389fee8989ef299489e0da5dd7066dc
vahidpaksharifi <52237876+vahidpaksharifi@users.noreply.github.com>
2026-07-24 14:00:47 +0330

    Update tablighat.php

--- tablighat.php
+++ tablighat.php
@@ -2,21 +2,21 @@
   "views": [
     {
       "name": "تبلیغ ۱",
-      "url": "https://raw.githubusercontent.com/vahidpaksharifi/movies.json/refs/heads/main/images/banners/banner_1784835567944.jpg",
+      "url": "https://your-correct-image-link-1.jpg",
       "flag": "banner",
       "IsADS": true,
       "time": "5"
     },
     {
-      "name": "تبلیغ ۲",
-      "url": "https://raw.githubusercontent.com/vahidpaksharifi/movies.json/refs/heads/main/images/banners/banner_1784835567944.jpg",
+      "name": "تبلیغ ۲", 
+      "url": "https://your-correct-image-link-2.png",
       "flag": "banner",
       "IsADS": true,
       "time": "4"
     },
     {
       "name": "تبلیغ ۳",
-      "url": "https://raw.githubusercontent.com/vahidpaksharifi/movies.json/refs/heads/main/images/banners/banner_1784835567944.jpg",
+      "url": "https://your-correct-image-link-3.webp", 
       "flag": "banner",
       "IsADS": true,
       "time": "6"
