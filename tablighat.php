8c5696c574cb84e877a2ba6024e4caa4e9ae8d4c
vahidpaksharifi <52237876+vahidpaksharifi@users.noreply.github.com>
2026-07-24 14:08:20 +0330

    Update tablighat.php

--- tablighat.php
+++ tablighat.php
@@ -1,25 +1,18 @@
 {
   "views": [
     {
-      "name": "تبلیغ ۱",
-      "url": "https://your-correct-image-link-1.jpg",
+      "name": "طبیعت گلستان",
+      "url": "https://images.unsplash.com/photo-1735358977553-2ab3c741ae6f?w=800",
       "flag": "banner",
       "IsADS": true,
       "time": "5"
     },
     {
-      "name": "تبلیغ ۲", 
-      "url": "https://your-correct-image-link-2.png",
+      "name": "تبلیغ ۲",
+      "url": "https://cdn.pixabay.com/photo/2023/.../tehran_1280.jpg",
       "flag": "banner",
       "IsADS": true,
       "time": "4"
-    },
-    {
-      "name": "تبلیغ ۳",
-      "url": "https://your-correct-image-link-3.webp", 
-      "flag": "banner",
-      "IsADS": true,
-      "time": "6"
     }
   ]
 }
